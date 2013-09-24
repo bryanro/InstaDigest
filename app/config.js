a927cf8dfb4580c45bd4977169045511e510a454
Bryan Rosenbaum <bryan.rosenbaum@gmail.com>
2013-09-23 21:07:21 -0400

    Config.js

---
+++ app/config.js
@@ -0,0 +1,27 @@
+﻿module.exports = {
+    development: {
+        mongoConn: 'mongodb://localhost/instadigest',
+        verbosityLevel: 'debug',
+        cookieExpiration: 86400000 * 30     // 30 days
+    },
+    beta: {
+        mongoConn: process.env.OPENSHIFT_MONGODB_DB_URL + 'instadigest',
+        verbosityLevel: 'debug',
+        cookieExpiration: 86400000 * 30     // 30 days
+    },
+    production: {
+        mongoConn: process.env.OPENSHIFT_MONGODB_DB_URL + 'instadigest',
+        verbosityLevel: 'warn',
+        cookieExpiration: 86400000 * 30     // 30 days
+    }
+}
+
+/*
+
+HOW TO SET ENVIRONMENT IN OPENSHIFT:
+
+1. SSH in
+2. Open ~/nodejs-0.6/conf/node.env
+3. Add line: export NODE_ENV=[development|beta|production]
+
+*/
