60aa57a98702ad2daafbb55ef08ffce49ad44100
Bryan Rosenbaum <bryan.rosenbaum@gmail.com>
2016-06-22 21:31:51 -0400

    Minor change to remove tailing / in version api endpoint

--- app/server/routes/status.js
+++ app/server/routes/status.js
@@ -5,7 +5,7 @@ router.get('/', function (req, res, next) {
     res.status(200).send('API is running');
 });
 
-router.get('/version/', function (req, res, next) {
+router.get('/version', function (req, res, next) {
     var packageDotJson = require('../../../package.json');
     res.status(200).send('Version ' + packageDotJson.version);
 });
